bc1934946e2e1f281142c697d52b407a4f3444ab
Erik <eriklf442@gmail.com>
2022-07-18 21:07:31 +0200

    More readable code. Also made everything const.

--- main.c
+++ main.c
@@ -8,17 +8,17 @@ int main()
 {
 	initscr();
 
-	WINDOW *main = subwin(stdscr, LINES - STDSCR_PADDING*2, COLS - STDSCR_PADDING*2, STDSCR_PADDING, STDSCR_PADDING);
+	const WINDOW *main = subwin(stdscr, LINES - STDSCR_PADDING*2, COLS - STDSCR_PADDING*2, STDSCR_PADDING, STDSCR_PADDING);
 
 	int rows, cols;
 	getmaxyx(main, rows, cols);
 
-	int available_hspace = cols - TEXT_TEXT_PADDING;
-	int available_vspace = rows - TEXT_LANG_PADDING;
+	const int available_hspace = cols - TEXT_TEXT_PADDING;
+	const int available_vspace = rows - TEXT_LANG_PADDING;
 
 	// TODO: Turn all this horrible math into functions.
 
-	int windows_width	= available_hspace / 2;
+	const int windows_width	= available_hspace / 2;
 
 	/*
 	 * We know that `available_vspace = textbox_height + langbox_height`
@@ -28,21 +28,21 @@ int main()
 	 * In other words: `langbox_height = available_vspace / (TEXT_TO_LANG_RATIO + 1)`
 	 */
 
-	int langbox_height	= available_vspace / (TEXT_TO_LANG_RATIO + 1);
-	int textbox_height	= TEXT_TO_LANG_RATIO * langbox_height;
+	const int langbox_height	= available_vspace / (TEXT_TO_LANG_RATIO + 1);
+	const int textbox_height	= TEXT_TO_LANG_RATIO * langbox_height;
+
+	const int main_origin		= STDSCR_PADDING;
+	const int dest_boxes_col	= STDSCR_PADDING + windows_width + TEXT_TEXT_PADDING;
+	const int textboxes_row		= STDSCR_PADDING + langbox_height + TEXT_LANG_PADDING;
 
 	WINDOW *src_langbox	= newwin(langbox_height, windows_width, 
-					STDSCR_PADDING, STDSCR_PADDING);
+					main_origin, main_origin);
 	WINDOW *dest_langbox	= newwin(langbox_height, windows_width, 
-					STDSCR_PADDING, 
-					STDSCR_PADDING + windows_width + TEXT_TEXT_PADDING);
-
+					main_origin, dest_boxes_col);
 	WINDOW *src_textbox	= newwin(textbox_height, windows_width, 
-					STDSCR_PADDING + langbox_height + TEXT_LANG_PADDING,
-					STDSCR_PADDING);
+					textboxes_row, main_origin);
 	WINDOW *dest_textbox	= newwin(textbox_height, windows_width,
-					STDSCR_PADDING + langbox_height + TEXT_LANG_PADDING,
-					STDSCR_PADDING + windows_width + TEXT_TEXT_PADDING);
+					textboxes_row, dest_boxes_col);
 
 	WINDOW *wins[WIN_AMOUNT] = { src_langbox, src_textbox, dest_langbox, dest_textbox };
 
