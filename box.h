4cdb66a001288246130f1937d1c0b265636196e6
Erik <eriklf442@gmail.com>
2022-07-19 17:16:34 +0200

    Separate header for box utilities.

---
+++ box.h
@@ -0,0 +1,75 @@
+#ifndef _BOX_H
+#define _BOX_H
+
+#include <ncurses.h>
+
+struct Box
+{
+	WINDOW *window;
+	chtype content[TEXT_CAP];
+	chtype *last_element;
+};
+
+void box_create(struct Box *b, WINDOW *window)
+{
+	b->window = window;
+
+	for (int i = 0; i < TEXT_CAP; i++)
+		b->content[i] = 0;
+
+	b->last_element = &(b->content[0]);
+}
+
+NCURSES_BOOL box_isfull(struct Box *box)
+{
+	return box->last_element + 2 == box->content + TEXT_CAP;
+}
+
+NCURSES_BOOL box_isempty(struct Box *box)
+{
+	return box->last_element == box->content;
+}
+
+
+void box_delchtype(struct Box *box)
+{
+	if (box_isempty(box)) {
+		endwin();
+		exit(1);
+	}
+
+	box->last_element--;
+	*(box->last_element) = 0;
+
+	int y, x;
+	getyx(box->window, y, x);
+	mvwdelch(box->window, y, x-1);
+}
+
+void box_addchtype(struct Box *box, chtype ch)
+{
+	if (box_isfull(box) == TRUE) {
+		endwin();
+		exit(1);
+	}
+
+	*(box->last_element) = ch;
+	box->last_element++;
+	waddch(box->window, ch);
+}
+
+void box_refreshwins(struct Box **boxes)
+{
+	for (int i = 0; i < WIN_AMOUNT; i++)
+		wnoutrefresh(boxes[i]->window);
+
+	doupdate();
+}
+
+void change_focused_box(struct Box **boxes, struct Box *focused_box, int *focused_box_idx)
+{
+	*focused_box_idx = (*focused_box_idx + 1) % WIN_AMOUNT;
+	focused_box = boxes[*focused_box_idx];
+}
+
+#endif
--- main.c
+++ main.c
@@ -2,80 +2,11 @@
 #include <ncurses.h>
 #include <assert.h>
 #include "config.h"
+#include "box.h"
 
 #define WIN_AMOUNT 4
 #define TEXT_CAP 12
 #define LANG_CAP 12
-
-struct Box
-{
-	WINDOW *window;
-	chtype content[TEXT_CAP];
-	chtype *last_element;
-};
-
-
-NCURSES_BOOL box_isfull(struct Box *box)
-{
-	return box->last_element + 2 == box->content + TEXT_CAP;
-}
-
-NCURSES_BOOL box_isempty(struct Box *box)
-{
-	return box->last_element == box->content;
-}
-
-
-void box_delchtype(struct Box *box)
-{
-	if (box_isempty(box)) {
-		endwin();
-		exit(1);
-	}
-
-	box->last_element--;
-	*(box->last_element) = 0;
-
-	int y, x;
-	getyx(box->window, y, x);
-	mvwdelch(box->window, y, x-1);
-}
-
-void box_addchtype(struct Box *box, chtype ch)
-{
-	if (box_isfull(box) == TRUE) {
-		endwin();
-		exit(1);
-	}
-
-	*(box->last_element) = ch;
-	box->last_element++;
-	waddch(box->window, ch);
-}
-
-void box_refreshwins(struct Box **boxes)
-{
-	for (int i = 0; i < WIN_AMOUNT; i++)
-		wnoutrefresh(boxes[i]->window);
-
-	doupdate();
-}
-
-void change_focused_box(struct Box **boxes, struct Box *focused_box, int *focused_box_idx)
-{
-	*focused_box_idx = (*focused_box_idx + 1) % WIN_AMOUNT;
-	focused_box = boxes[*focused_box_idx];
-}
-
-void create_box(struct Box *b, WINDOW *window)
-{
-	b->window = window;
-
-	for (int i = 0; i < TEXT_CAP; i++)
-		b->content[i] = 0;
-
-	b->last_element = &(b->content[0]);
-}
 
 void show_debug_win(WINDOW *debug_window, chtype *text)
 {
@@ -127,10 +58,10 @@ int main()
 	const int textboxes_row		= STDSCR_PADDING + langbox_height + TEXT_LANG_PADDING;
 
 	struct Box src_langbox, src_textbox, dest_langbox, dest_textbox;
-	create_box(&src_langbox, newwin(langbox_height, windows_width, main_origin, main_origin));
-	create_box(&src_textbox, newwin(langbox_height, windows_width, main_origin, dest_boxes_col));
-	create_box(&dest_langbox, newwin(textbox_height, windows_width, textboxes_row, main_origin));
-	create_box(&dest_textbox, newwin(textbox_height, windows_width, textboxes_row, dest_boxes_col));
+	box_craete(&src_langbox, newwin(langbox_height, windows_width, main_origin, main_origin));
+	box_craete(&src_textbox, newwin(langbox_height, windows_width, main_origin, dest_boxes_col));
+	box_craete(&dest_langbox, newwin(textbox_height, windows_width, textboxes_row, main_origin));
+	box_craete(&dest_textbox, newwin(textbox_height, windows_width, textboxes_row, dest_boxes_col));
 
 	struct Box *boxes[WIN_AMOUNT] = {&src_langbox, &src_textbox, &dest_langbox, &dest_textbox};
 
